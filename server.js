dc8bf7d034b762748e43e60519293cf808d89e7c
Sunil Kanamadi <sunilkanamadi1999@gmail.com>
2026-07-02 15:00:08 +0530

    updated the server file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const distFolder = path.join(__dirname, 'dist/Library-App/browser');
+
+app.use(express.static(distFolder));
+
+app.use((req, res) => {
+  res.sendFile(path.join(distFolder, 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
